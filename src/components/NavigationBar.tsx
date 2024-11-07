0d49e91b42e4b71a1884db52d837fd963bd9bdb9
pwv2 <pwv2@outlook.com>
2024-11-07 09:10:17 +0800

    Optimise connect wallet modal on mobile devices

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { usePathname } from 'next/navigation'
+import { usePathname } from "next/navigation";
 import {
   Box,
   Container,
@@ -14,60 +14,69 @@ import { useDisclosure, useMediaQuery } from "@mantine/hooks";
 import LogoSVG from "../assets/logo.svg";
 
 import { IconMenu2 } from "@tabler/icons-react";
-import {useConnectWallet, useCurrentAddress, useWallets} from '@roochnetwork/rooch-sdk-kit'
-import {shortAddress} from '../utils/address'
-import {WalletConnectModal} from './connect-model'
-import {useState} from 'react'
+import { useCurrentAddress } from "@roochnetwork/rooch-sdk-kit";
+import { shortAddress } from "../utils/address";
+import { WalletConnectModal } from "./connect-model";
+import { useState } from "react";
 
 function DesktopNavigationBar({ style }: { style?: Object }) {
-  const currentAddress = useCurrentAddress()
+  const currentAddress = useCurrentAddress();
   const pathname = usePathname();
 
-  const [showConnectModel, setShowConnectModel] = useState(false)
+  const [showConnectModel, setShowConnectModel] = useState(false);
 
   return (
-     <Box style={style}>
-       <WalletConnectModal isOpen={showConnectModel} onClose={()=>setShowConnectModel(false)}/>
-       <Container size="lg">
-         <Flex py="md" align="center" gap="lg">
-           <Link href="/">
-             <LogoSVG width={80} />
-           </Link>
-           <Anchor
-             component={Link}
-             href="/"
-             c="dark"
-             underline="never"
-             fw={pathname === "/" ? "500" : "400"}
-           >
-             Home
-           </Anchor>
-           <Anchor
-             component={Link}
-             href="/stake"
-             c="dark"
-             underline="never"
-             fw={pathname === "/grow" ? "500" : "400"}
-           >
-             Get $GROW
-           </Anchor>
-           <Anchor
-             component={Link}
-             href=""
-             c="dark"
-             underline="never"
-             fw={pathname === "" ? "500" : "400"}
-           >
-             Ideas
-           </Anchor>
-           <Button radius="md" ml="auto" onClick={() => {
-             setShowConnectModel(currentAddress === undefined)
-           }}>
-             {currentAddress ? shortAddress(currentAddress.toStr()) : 'Connect Wallet'}
-           </Button>
-         </Flex>
-       </Container>
-     </Box>
+    <Box style={style}>
+      <WalletConnectModal
+        isOpen={showConnectModel}
+        onClose={() => setShowConnectModel(false)}
+      />
+      <Container size="lg">
+        <Flex py="md" align="center" gap="lg">
+          <Link href="/">
+            <LogoSVG width={80} />
+          </Link>
+          <Anchor
+            component={Link}
+            href="/"
+            c="dark"
+            underline="never"
+            fw={pathname === "/" ? "500" : "400"}
+          >
+            Home
+          </Anchor>
+          <Anchor
+            component={Link}
+            href="/stake"
+            c="dark"
+            underline="never"
+            fw={pathname === "/grow" ? "500" : "400"}
+          >
+            Get $GROW
+          </Anchor>
+          <Anchor
+            component={Link}
+            href=""
+            c="dark"
+            underline="never"
+            fw={pathname === "" ? "500" : "400"}
+          >
+            Ideas
+          </Anchor>
+          <Button
+            radius="md"
+            ml="auto"
+            onClick={() => {
+              setShowConnectModel(currentAddress === undefined);
+            }}
+          >
+            {currentAddress
+              ? shortAddress(currentAddress.toStr())
+              : "Connect Wallet"}
+          </Button>
+        </Flex>
+      </Container>
+    </Box>
   );
 }
 
@@ -75,8 +84,15 @@ function MobileNavigationBar({ style }: { style?: Object }) {
   const [opened, { open, close }] = useDisclosure(false);
   const pathname = usePathname();
 
+  const currentAddress = useCurrentAddress();
+  const [showConnectModel, setShowConnectModel] = useState(false);
+
   return (
     <Box style={style}>
+      <WalletConnectModal
+        isOpen={showConnectModel}
+        onClose={() => setShowConnectModel(false)}
+      />
       <Container size="lg">
         <Flex py="md" align="center" gap="lg">
           <Link href="/">
@@ -91,7 +107,16 @@ function MobileNavigationBar({ style }: { style?: Object }) {
             <IconMenu2 />
           </UnstyledButton>
 
-          <Button radius="md">Connect Wallet</Button>
+          <Button
+            radius="md"
+            onClick={() => {
+              setShowConnectModel(currentAddress === undefined);
+            }}
+          >
+            {currentAddress
+              ? shortAddress(currentAddress.toStr())
+              : "Connect Wallet"}
+          </Button>
         </Flex>
       </Container>
 
--- src/components/connect-model.tsx
+++ src/components/connect-model.tsx
@@ -1,29 +1,46 @@
-import React from 'react';
-import {Modal, Button, Text, Group, Stack} from '@mantine/core'
-import {useConnectWallet, useWallets, Wallet} from '@roochnetwork/rooch-sdk-kit'
+import React from "react";
+import { Modal, Button, Stack } from "@mantine/core";
+import {
+  useConnectWallet,
+  useWallets,
+  Wallet,
+} from "@roochnetwork/rooch-sdk-kit";
 interface WalletConnectModalProps {
-	isOpen: boolean;
-	onClose: () => void;
+  isOpen: boolean;
+  onClose: () => void;
 }
-export const WalletConnectModal: React.FC<WalletConnectModalProps> = ({ isOpen, onClose }) => {
-	const wallets = useWallets();
-	const {mutateAsync: connectWallet} = useConnectWallet()
-	const handleConnectWallet = async (wallet: Wallet)  => {
-		await connectWallet({wallet})
-		onClose();
-	};
+export const WalletConnectModal: React.FC<WalletConnectModalProps> = ({
+  isOpen,
+  onClose,
+}) => {
+  const wallets = useWallets();
+  const { mutateAsync: connectWallet } = useConnectWallet();
+  const handleConnectWallet = async (wallet: Wallet) => {
+    await connectWallet({ wallet });
+    onClose();
+  };
 
-	return (
-		<Modal
-			opened={isOpen}
-			onClose={onClose}
-			title="Choose your preferred wallet to connect:"
-		>
-			<Stack mt="md">
-				{
-					wallets.filter((wallet) => wallet.getName() !== 'OneKey').map((wallet) => <Button key={wallet.getName()} onClick={() => handleConnectWallet(wallet)}>{wallet.getName()}</Button>)
-				}
-			</Stack>
-		</Modal>
-	);
+  return (
+    <Modal
+      opened={isOpen}
+      onClose={onClose}
+      title="Choose your preferred wallet to connect:"
+      radius="md"
+      yOffset="25vh"
+    >
+      <Stack mt="xs" gap="sm">
+        {wallets
+          .filter((wallet) => wallet.getName() !== "OneKey")
+          .map((wallet) => (
+            <Button
+              radius="md"
+              key={wallet.getName()}
+              onClick={() => handleConnectWallet(wallet)}
+            >
+              {wallet.getName()}
+            </Button>
+          ))}
+      </Stack>
+    </Modal>
+  );
 };
