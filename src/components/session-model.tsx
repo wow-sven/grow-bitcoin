dad10149739b2cb6eaff1566875de1fcfbc3c750
sven <sven.tan@icloud.com>
2024-11-24 17:18:56 +0800

    add Rgas hint

--- src/components/session-model.tsx
+++ src/components/session-model.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react'
 import {Modal, Button, Stack} from '@mantine/core'
-import {useCreateSessionKey} from '@roochnetwork/rooch-sdk-kit'
+import {useCreateSessionKey, useCurrentAddress, useCurrentNetwork} from '@roochnetwork/rooch-sdk-kit'
 import {useNetworkVariable} from '../app/networks'
 interface CreateSessionModalProps {
 	isOpen: boolean;
@@ -9,15 +9,33 @@ interface CreateSessionModalProps {
 export const CreateSessionModal: React.FC<CreateSessionModalProps> = ({ isOpen, onClose }) => {
 	const {mutateAsync: createSessionKey} = useCreateSessionKey()
 	const contractAddr = useNetworkVariable('contractAddr')
+	const network = useCurrentNetwork()
+	const [error, setError] = useState(0)
+	const addr = useCurrentAddress()
 
 	const handleCreateSession = async () => {
-		await createSessionKey({
-			appName: 'Rooch GROW',
-			appUrl: 'https://test-grow.rooch.network',
-			scopes: [`${contractAddr}::*::*`],
-			maxInactiveInterval: 12000
-		})
-		onClose();
+
+		if (error !== 0) {
+			const tag = network === 'mainnet' ? '' : 'test-'
+			window.open(`https://${tag}portal.rooch.network/faucet/${addr?.toStr()}`)
+			setError(0)
+			return
+		}
+
+		try {
+			await createSessionKey({
+				appName: 'Rooch GROW',
+				appUrl: 'https://test-grow.rooch.network',
+				scopes: [`${contractAddr}::*::*`],
+				maxInactiveInterval: 12000
+			})
+			onClose();
+		}catch (e: any) {
+			if (e.code === 1004) {
+				setError(e.code)
+			}
+			console.log(e.code)
+		}
 	};
 
 	return (
@@ -26,9 +44,9 @@ export const CreateSessionModal: React.FC<CreateSessionModalProps> = ({ isOpen, 
 			onClose={onClose}
 			title="Session Required"
 		>
-			<text>Create a stake session</text>
+			<text>{error !== 0 ? 'RGas not sufficient funds':'Create a stake session'}</text>
 			<Stack mt="md">
-				<Button onClick={handleCreateSession}>Create</Button>
+				<Button onClick={handleCreateSession}>{error !== 0 ? 'Click To Faucet' : 'Create'}</Button>
 			</Stack>
 		</Modal>
 	);
