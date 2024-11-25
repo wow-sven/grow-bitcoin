c72dc514488e2dfbe29da3dbff9cc2fc9c637ea5
sven Tan <sven.tan@icloud.com>
2024-11-25 19:43:17 +0800

    fix (#20)

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,4 +1,5 @@
 import ClientProjectsPage from "./client-page";
+
 import {getXAvatar} from '@/utils/x'
 
 export default async function Projects() {
@@ -15,16 +16,20 @@ export default async function Projects() {
 
   const projects = projectsRawData.records.reduce((a: Project[], c: any) => {
     if (c.fields.Show) {
-      const { fields } = c;
-      a.push({
-        id: c.id,
-        slug: fields.Slug,
-        name: fields.Name,
-        avatar: getXAvatar(fields.Twitter),
-        thumbnail: fields.Logo?.[0].thumbnails.large.url,
-        oneLiner: fields["One-Liner"],
-        tags: fields.Tags || [],
-      });
+      try {
+        const { fields } = c;
+        a.push({
+          id: c.id,
+          slug: fields.Slug,
+          name: fields.Name,
+          icon: `https://unavatar.io/x/${fields.Twitter.slice(fields.Twitter.lastIndexOf('/')+1)}`,
+          thumbnail: fields.Logo?.[0].thumbnails.large.url,
+          oneLiner: fields["One-Liner"],
+          tags: fields.Tags || [],
+        });
+      } catch (e) {
+        console.log(e)
+      }
     }
     return a;
   }, []);
