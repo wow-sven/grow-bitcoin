625567fa8e4186cc11a6573f539b3eaa01ea3862
King <lispking@qq.com>
2024-12-20 12:31:42 +0800

    feat: support set maxGas for batch handler (#45)
    * feat: support set maxGas for batch handler
    
    * make code tidy

--- src/components/stake-card.tsx
+++ src/components/stake-card.tsx
@@ -183,7 +183,7 @@ export const StakeCard: React.FC<StakeCardProps> = ({ target, assets }) => {
           args: [Args.vec('objectId', utxoIds)],
         })
     }
-    tx.setMaxGas(10000000)
+    tx.setMaxGas(utxoIds.length * 0.023 * 100000000) // len * 0.023 * 1RGas
 
     try {
       const result = await signAndExecuteTransaction({
